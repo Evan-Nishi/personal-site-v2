6d4f6042bda70639c76e507e0d2301e5397f8a3c
pErs0nZ <evankn.13@gmail.com>
2020-12-17 14:54:51 -0800

    coffee gallery added

---
+++ src/pages/coffee.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Button from '../components/Button'
+import Card from '../components/Card'
+
+export default class Coffee extends React.Component{
+    render(){
+        return(
+            <Layout Title="coffee">
+                <div className="homeButton">
+                    <Button href="/">Home</Button>
+                </div>
+                <h1 id="title">Coffee Gallery</h1>
+                <Card>
+                    <h3>With Computer</h3>
+                    <img id="gallery" src="/dev.png"/>
+                </Card>
+                <br/>
+                <br/>
+                <Card>
+                    <h3>Original</h3>
+                    <img id="gallery" src="/normal coffee.png"/>
+                </Card>
+                <br/>
+                <br/>
+                <Card>
+                    <h3>Halloween 2020</h3>
+                    <img id="gallery" src="/spoopy coffee.png"/>
+                </Card>
+                <br/>
+                <br/>
+                <Card>
+                    <h3>Thanksgiving 2020</h3>
+                    <img id="gallery" src="/thanksgiving coffee.png"/>
+                </Card>
+                <br/>
+                <br/>
+                <Card>
+                    <h3>Christmas 2020</h3>
+                    <img id="gallery" src="/christmas coffee.png"/>
+                </Card>
+            </Layout>
+        )
+    }
+}
